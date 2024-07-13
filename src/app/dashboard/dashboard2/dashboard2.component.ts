57ab69f52b84ac70e0a5f62d41e5bcd9792b96ab
souadlengliz <souad.lengliz@esprit.tn>
2024-07-13 01:14:33 +0100

    Pie chart Dashboard 2

--- src/app/dashboard/dashboard2/dashboard2.component.ts
+++ src/app/dashboard/dashboard2/dashboard2.component.ts
@@ -244,6 +244,35 @@ export class Dashboard2Component {
       fullWidth: true
     },
   };
+  EventParticipationRatePie: Chart = {
+    type: 'Pie',
+    data: data['EventParticipationRatePie'],
+    options: {
+      donut: true,
+      startAngle: 0,
+      labelInterpolationFnc: function (value) {
+        var total = data['EventParticipationRatePie'].series.reduce(function (prev, series) {
+          return prev + series.value;
+        }, 0);
+        return total + '%';
+      }
+    },
+    events: {
+      draw(data: any): void {
+        if (data.type === 'label') {
+          if (data.index === 0) {
+            data.element.attr({
+              dx: data.element.root().width() / 2,
+              dy: data.element.root().height() / 2
+            });
+          } else {
+            data.element.remove();
+          }
+        }
+
+      }
+    }
+  };
   calculateGrowthRate(): number[] {
     const series = data["EventGrowthRate"].series[0];
     const growthRates: number[] = [];
